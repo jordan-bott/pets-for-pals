7a3e7d81a16b297a89e4f93829e7a9edfe51e0f0
jordan-bott <jordanbott.dev@gmail.com>
2024-02-25 15:13:09 -0800

    standard eslint file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,6 @@
 import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me Image"),
-//     React.createElement(Pet, {
-//       animal: "Dog",
-//       name: "Mitzie",
-//       breed: "Princess",
-//     }),
-//     React.createElement(Pet, {
-//       animal: "Gerbil",
-//       name: "John",
-//       breed: "Gerbil",
-//     }),
-//     React.createElement(Pet, { animal: "Cat", name: "Kuichi", breed: "Mixed" }),
-//   ]);
-// };
-
 const App = () => {
   return (
     <div>
