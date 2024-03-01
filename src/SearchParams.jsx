8ecc1c815a7e8a0db47c3de407d97b1f529281b8
jordan-bott <jordanbott.dev@gmail.com>
2024-02-29 21:12:46 -0800

    Results component, styles Pet Component

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
 import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -75,9 +75,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((p) => (
-        <Pet name={p.name} animal={p.animal} breed={p.breed} key={p.id} />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
