ebe32df2e6c51efdf27b84d8ecb231420872f7de
jordan-bott <jordanbott.dev@gmail.com>
2024-03-03 01:07:44 -0800

    adds React Query, writes fetchPet query

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // components
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClientProvider>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
---
+++ src/fetchPet.js
@@ -0,0 +1,14 @@
+const fetchPet = async ({ queryKey }) => {
+  const id = queryKey[1];
+
+  const apiRes = await fetch(`http://[ets-v2.dev-apis.com/pets?id=${id}`);
+
+  if (!apiRes.ok) {
+    throw new Error(`details/${id} fetch not okay`);
+  }
+
+  // you don't have to await here, if you do, it adds a "tick" (a moment of latency)
+  return apiRes.json();
+};
+
+export default fetchPet;
